e7cdaa14f4a14a8ce5908785207a9d084dc089f2
Jerrard Smith <61473803+JerrardTSmith@users.noreply.github.com>
2024-11-07 23:26:47 -0500

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -9,19 +9,19 @@ type Props = {
 const Layout = ({ children, title = "This is the default title" }: Props) => (
   <div className="relative min-h-screen flex flex-col justify-center items-center bg-hero bg-cover">
     <Head>
-      <title>{title} | Themeptation</title>
+      <title>{title} | South Atlantic Technology Partners</title>
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       <meta
         name="description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="Successful Businesses Know That Those Who Go Fast Go Together. We're Virginia's Premiere Web Development and Business Automation Solutions Provider."
       />
       <meta property="og:title" content={`${title} | Themeptation`} />
       <meta
         property="og:description"
-        content="Slioth - is a One Page Saas Lading Page build with Tailwind CSS and NextJs created by themeptation"
+        content="We're Virginia's Premiere Web Development and Business Automation Solutions Provider."
       />
-      <meta property="og:url" content="https://slioth.themepttation.net/" />
+      <meta property="og:url" content="https://www.southatlantictech.com/" />
       <meta property="og:type" content="website" />
     </Head>
     {children}
